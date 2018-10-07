a788df8c5b2f7c2471ce67ca4ec0bac0da887fb7
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-10-07 14:44:52 +0900

    데이터클래스로 파싱하는법

---
+++ book_XML_Parsing_Data_Class/BookData.swift
@@ -0,0 +1,13 @@
+//
+//  BookData.swift
+//  book_XML_Parsing_Data_Class
+//
+//  Created by D7702_10 on 2018. 10. 1..
+//  Copyright © 2018년 ksh. All rights reserved.
+//
+
+import Foundation
+class BookData {
+    var title = ""
+    var author = ""
+}
